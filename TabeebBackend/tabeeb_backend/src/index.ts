51933de8ee104f50ce8e9ae85e068750a0564cd2
Moeez Ahmed <126664082+moeez4316@users.noreply.github.com>
2025-07-21 19:06:53 +0500

    Added schema for postgres sql using neon, added the doctor and patient controllers and routes

--- TabeebBackend/tabeeb_backend/src/index.ts
+++ TabeebBackend/tabeeb_backend/src/index.ts
@@ -3,6 +3,8 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db';
 import recordRoutes from './routes/medicalRecords';
+import doctorRoutes from './routes/doctorRoutes';
+import patientRoutes from './routes/patientRoutes';
 
 dotenv.config();
 connectDB();
@@ -11,7 +13,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use('/api/doctor', doctorRoutes); 
+app.use('/api/patient', patientRoutes);
 app.use('/api/records', recordRoutes);
+
 
 const PORT = process.env.PORT || 5002;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
